4e001831f228702fabc7fd08f10f7003e1364ac7
Ankur reyes <Ankur reyes>
2016-10-21 00:12:03 +0530

    yes

--- app.js
+++ app.js
@@ -12,32 +12,55 @@ let MONGO_URL = "mongodb://ankur1163:lightbulb1@ds013946.mlab.com:13946/ank1163"
 
 
 var app = express();
+//app.listen(5000);
 app.listen(process.env.PORT || 5000);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-//app.listen(3000);
-/*
-mongo.connect(MONGO_URL, function (error, db) {
-    if (error) {
-    throw new Error('Database failed to connect!');
-    } else {
-    console.log('MongoDB successfully connected on port 27017.');
-    }
-    // start the server
-    app.listen(3000, function() {
-        console.log('Express server listening on port 3000');
-    });
+mongoose.connect(MONGO_URL);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback () {
+
+  // Create song schema
+  var songSchema = mongoose.Schema({
+    decade: String,
+    artist: String,
+    song: String,
+    weeksAtOne: Number
+  });
+
+   var Song = mongoose.model('songs', songSchema);
+   var seventies = new Song({
+   decade: '1970s',
+   artist: 'Debby Boone',
+   song: 'You Light Up My Life',
+   weeksAtOne: 10
+ });
+ var ninenine = new Song({
+   decade:"1800s",
+   artist:"adnan sami",
+   song:"its awesome",
+   weeksAtOne:50
+
+ }
+
+
+
+ );
+
+ var eighties = new Song({
+   decade: '1980s',
+   artist: 'Olivia Newton-John',
+   song: 'Physical',
+   weeksAtOne: 10
+ });
+
+
+ seventies.save();
+ eighties.save();
+ ninenine.save();
 });
-MONGO_URL.Book.insert({
- title:"refactor the dom",
- author:"ty he",
- category:"tech"
-})
-MONGO_URL.Book.insert({
- title:"1",
- author:"2e",
- category:"3"
-})
-*/
